bce9ecd438db357ae741a4729112a33d44ad9e7e
Jom Dollesin <codewithjom@gmail.com>
2023-03-01 05:23:49 +0800

    feat: added an AddPost component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import AddPost from '../components/AddPost'
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-6xl">hello?</h1>
+      <AddPost />
     </main>
   )
 }
