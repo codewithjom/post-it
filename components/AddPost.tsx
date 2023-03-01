561b12a57c91be3c5be5943a23c8c116eb8c82e3
Jom Dollesin <codewithjom@gmail.com>
2023-03-01 11:33:38 +0800

    feat: added a query wrapper for the api.

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -1,24 +1,44 @@
 'use client'
 
 import { useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
 
 export default function CreatePost() {
   const [title, setTitle] = useState('')
   const [isDisabled, setIsDisabled] = useState(false)
+
+  const { mutate } = useMutation(async (title: string) => await axios.post('/api/posts/addPost', { title }), {
+    onError: error => {
+      console.log(error)
+    },
+    onSuccess: data => {
+      console.log(data)
+      setTitle('')
+      setIsDisabled(false)
+    }
+  })
+
+  const submitPost = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setIsDisabled(true)
+    mutate(title)
+  }
+
   return (
-    <form className="bg-white my-8 p-8 rouned-md">
+    <form onSubmit={submitPost} className="p-8 my-8 bg-white rouned-md">
       <div className="flex flex-col my-4">
         <textarea
           onChange={e => setTitle(e.target.value)}
           name="title"
           value={title}
           placeholder="What's on your mind?"
-          className="p-4 text-lg rounded-md my-2 bg-gray-200"
+          className="p-4 my-2 text-lg bg-gray-200 rounded-md"
         ></textarea>
       </div>
       <div className="flex items-center justify-between gap-2">
         <p className={`font-bold text-sm ${title.length > 300 ? 'text-red-700' : 'text-gray-700'}`}>{`${title.length}/300`}</p>
-        <button disabled={isDisabled} className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25">
+        <button disabled={isDisabled} type="submit" className="px-6 py-2 text-sm text-white bg-teal-600 rounded-xl disabled:opacity-25">
           Create a post
         </button>
       </div>
