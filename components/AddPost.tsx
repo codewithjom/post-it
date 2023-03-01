158562878ee84f465538d93ff963542e5ab39635
Jom Dollesin <codewithjom@gmail.com>
2023-03-02 07:56:09 +0800

    fix(AddPost): added queryClient to invalidate queries.

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -8,6 +8,7 @@ import toast from 'react-hot-toast'
 export default function CreatePost() {
   const [title, setTitle] = useState('')
   const [isDisabled, setIsDisabled] = useState(false)
+  const queryClient = useQueryClient()
   let toastPostID: string
 
   const { mutate } = useMutation(async (title: string) => await axios.post('/api/posts/addPost', { title }), {
@@ -19,6 +20,7 @@ export default function CreatePost() {
     },
     onSuccess: data => {
       toast.success('Post has been made 🔥', { id: toastPostID })
+      queryClient.invalidateQueries(['posts'])
       setTitle('')
       setIsDisabled(false)
     }
